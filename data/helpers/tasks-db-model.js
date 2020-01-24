2b3499780cabdff5134d52165fff0c7971861e67
Evans Ibok <evansibok@gmail.com>
2020-01-24 12:42:20 +0100

    create addTasks db access and router to add task to a project

--- data/helpers/tasks-db-model.js
+++ data/helpers/tasks-db-model.js
@@ -4,20 +4,14 @@ function getAllTasks() {
   return db('tasks as t')
     .innerJoin('projects as p', 'p.id', 't.project_id')
     .select('t.id as task_id', 't.description as task_description', 'notes as task_notes', 't.completed as isTaskCompleted', 'project_name', 'p.description as project_description')
+}
 
-
-  // select
-  //    t.id as task_id,
-  //   t.description as task_description,
-  //   notes as task_notes,
-  //   t.completed as isTaskCompleted,
-  //   project_name,
-  //   p.description as project_description
-  // from tasks t
-  // join projects p
-  // on p.id = t.project_id;
+function addTasks(newTask) {
+  return db('tasks')
+    .insert(newTask, 'project_id')
 }
 
 module.exports = {
-  getAllTasks
+  getAllTasks,
+  addTasks
 }
--- data/routes/projects-router.js
+++ data/routes/projects-router.js
@@ -1,6 +1,7 @@
 const express = require('express');
 
 const projectsDb = require('../helpers/projects-db-model');
+const tasksDb = require('../helpers/tasks-db-model');
 
 const router = express.Router();
 
@@ -39,6 +40,23 @@ router.post('/', (req, res) => {
   projectsDb.addProject(newProject)
     .then(data => {
       res.status(201).json(data);
+    })
+    .catch(err => {
+      res.status(500).json({
+        errorMessage: err.message,
+        stack: err.stack
+      });
+    })
+});
+
+router.post('/:id/tasks', (req, res) => {
+  const { id } = req.params;
+  const newTask = req.body
+
+  const updatedTask = { ...newTask, project_id: id }
+  tasksDb.addTasks(updatedTask)
+    .then(tasks => {
+      res.status(200).json(tasks);
     })
     .catch(err => {
       res.status(500).json({
